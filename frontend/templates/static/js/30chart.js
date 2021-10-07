94c503f456e0474b574cb2788718333ba7568faf
Amrit Thakkar <amritthakkar24@gmail.com>
2021-10-07 22:48:41 +0530

    update

--- frontend/templates/static/js/30chart.js
+++ frontend/templates/static/js/30chart.js
@@ -1,9 +1,9 @@
-var data = [12, 19, 3];
+var data = [12, 19, 3, 12, 45, 43, 19, 34, 54,122, 30, 23];
 var ctx = document.getElementById('my_Chart').getContext('2d');
 var my_Chart = new Chart(ctx, {
  type: 'line' ,
     data: {
-        labels: ['Savings', 'Expenses', 'Transaction'],
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         datasets: [{
             label: 'Monthly Tracker',
             data: data,
